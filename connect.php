dd810e8ff0610b959789648ac160b55fe37f7d9b
dddaria <daria.lewandowska24@gmail.com>
2021-02-25 17:02:13 +0100

    reserve-function moved from connect to config

--- connect.php
+++ connect.php
@@ -7,24 +7,7 @@ $dbConn = mysqli_connect($host, $user, $password, $database);
 if($dbConn->connect_errno) {
     echo "Failed to connect to database:" . $dbConn->connect_error;
 };
-//echo "Connected successfully";
-
-//Function for reserving books
-function reserve($reserve,$BookID){
-    include ('config.php');
-    // Create connection
-    $dbConn = mysqli_connect($host, $user, $password, $database);
-    //Check connection
-    if($dbConn->connect_errno) {
-        echo "Failed to connect to database:" . $dbConn->connect_error;
-    }; 
- 
-    $sql = "UPDATE Book SET Status='{$reserve}' WHERE BookID='{$BookID}'";
-    if ($dbConn->query($sql) === FALSE) {
-        echo "Error updating record: " . $dbConn->error;
-    }
-    $dbConn->close();
-}  
+//echo "Connected successfully"; 
 
 ?>
 
